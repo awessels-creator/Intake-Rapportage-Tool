fbccebac18589ba66f346b76332001a43fbcf8f4
Wouter Wessels <wouter.wessels@meppel.nl>
2026-06-21 15:45:45 +0200

    fix: docx download met node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import babel from '@rolldown/plugin-babel'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: process.env.VITE_BASE_URL || '/Intake-Rapportage-Tool/',
   plugins: [
     tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    nodePolyfills({
+      include: ['buffer', 'stream', 'util', 'process'],
+    }),
   ],
 })
